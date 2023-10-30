64b2662f68aeb4c65b40e003814d5ce992b7f39e
Kiril-Mordan <kiril.mordan@icloud.com>
2023-10-30 04:52:12 +0100

    [REFACTOR] utility class that records parent div and extracts text from it for new links

--- python_modules/search_based_extractor.py
+++ python_modules/search_based_extractor.py
@@ -1,5 +1,15 @@
+"""
+Search Based Extractor
+
+Utility to simplify webscraping by taking advantave of search and assumptions about html structure.
+Extractor allows to find parent html element that contains searched term, record path to it in a file
+and reuse that to scrape data with same html structure.
+"""
+
+
 # Imports
 ## essential
+import json
 import logging
 import attr
 ## requests
@@ -8,93 +18,225 @@ import requests
 from bs4 import BeautifulSoup, Tag, Comment
 
 
-def extract_visible_text(soup):
-    """
-    Extracts visible text from a BeautifulSoup object.
-    """
-    texts = []
+@attr.s
+class SearchBasedExtractor:
+    # pylint: disable=too-many-instance-attributes
 
-    for element in soup.recursiveChildGenerator():
-        if isinstance(element, Comment):  # Skip comments
-            continue
-        if isinstance(element, Tag):
-            if element.name in ["script", "style"]:  # Skip script/style tags
+    """
+    A utility class designed to extract specific content from web pages based on search terms.
+
+    Attributes:
+        soup (BeautifulSoup): A BeautifulSoup object representing the parsed HTML.
+        filename (str): Name of the file to save or load paths.
+        logger (logging.Logger): A logger instance for logging messages.
+        logger_name (str): Name assigned to the logger.
+        loggerLvl (int): Logging level.
+        link (str): The web link from which the soup is generated.
+        found_element (Tag): The element containing the searched text.
+        path (list): List of tuples representing the path to the found_element.
+        searched_text (str): Text that was searched.
+    """
+
+    soup = attr.ib(default=None)
+    filename = attr.ib(default='recorded_sbe.json')
+
+    logger = attr.ib(default=None)
+    logger_name = attr.ib(default='SearchBasedExtractor')
+    loggerLvl = attr.ib(default=logging.INFO)
+
+    link = attr.ib(default=None, init=False)
+    found_element = attr.ib(default=None, init=False)
+    path = attr.ib(default=[], init=False)
+    searched_text = attr.ib(default='', init=False)
+
+
+    def __attrs_post_init__(self):
+        self.initialize_logger()
+
+    def initialize_logger(self):
+
+        """
+        Initialize a logger for the class instance based on
+        the specified logging level and logger name.
+        """
+
+        if self.logger is None:
+            logging.basicConfig(level=self.loggerLvl)
+            logger = logging.getLogger(self.logger_name)
+            logger.setLevel(self.loggerLvl)
+
+            self.logger = logger
+
+    def initialize_soup(self, link : str, reset : bool = False):
+
+        """
+        Initialize a soup for the class instance based on the specified link.
+        """
+
+        if (self.soup is None) or reset:
+
+            if link:
+                try:
+                    response = requests.get(link, timeout=(5,15))
+                    response.raise_for_status()
+                except Exception as e:
+                    self.logger.error("Provided link was unreachable!")
+                    print(e)
+
+            else:
+                self.logger.error("Link is missing!")
+                self.logger.error("Either provide 'soup' parameter or 'link' parameter!")
+                raise ValueError("Missing 'link' parameter!")
+
+            self.soup = BeautifulSoup(response.content, 'html.parser')
+            self.link = link
+
+
+    def extract_visible_text(self, new_line_separator : str = '\n'):
+        """
+        Extracts visible text from a BeautifulSoup object.
+        """
+        texts = []
+
+        for element in self.soup.recursiveChildGenerator():
+            if isinstance(element, Comment):  # Skip comments
                 continue
-        if isinstance(element, str):  # NavigableString
-            stripped = element.strip()
-            if stripped:
-                texts.append(stripped)
+            if isinstance(element, Tag):
+                if element.name in ["script", "style"]:  # Skip script/style tags
+                    continue
+            if isinstance(element, str):  # NavigableString
+                stripped = element.strip()
+                if stripped:
+                    texts.append(stripped + new_line_separator)
 
-    return ' '.join(texts)
+        return ' '.join(texts)
 
-def find_element_by_text(soup, search_text : str):
-    """
-    Finds the first element containing the search_text and returns the path to it.
-    """
-    path = []
-    found_element = None
+    def find_path_with_text(self, search_text : str):
+        """
+        Finds the first element containing the search_text and returns the path to it.
+        """
+        path = []
+        found_element = None
 
-    def helper(element, search_text):
-        nonlocal found_element
-        if isinstance(element, Tag):
-            siblings = [sib for sib in element.previous_siblings if isinstance(sib, Tag) and sib.name == element.name]
+        def helper(element, search_text):
+            nonlocal found_element
+            if isinstance(element, Tag):
+                siblings = [sib for sib in element.previous_siblings \
+                    if isinstance(sib, Tag) and sib.name == element.name]
 
-            position = len(siblings)
-            path.append((element.name, position))
+                position = len(siblings)
+                path.append((element.name, position))
 
-            for child in element.children:
-                found = helper(child, search_text)
-                if found:
+                for child in element.children:
+                    found = helper(child, search_text)
+                    if found:
+                        return True
+
+                path.pop()  # Remove the current tag from the path if text not found in this subtree
+
+            if isinstance(element, str):  # NavigableString
+                stripped = element.strip()
+                if search_text in stripped:
+                    found_element = element.parent  # save the parent tag
                     return True
 
-            path.pop()  # Remove the current tag from the path if text not found in this subtree
+            return False
 
-        if isinstance(element, str):  # NavigableString
-            stripped = element.strip()
-            if search_text in stripped:
-                found_element = element.parent  # save the parent tag
-                return True
+        found = helper(self.soup, search_text)
 
-        return False
-
-    found = helper(soup, search_text)
-    return found_element, path if found else (None, [])
-
-def extract_from_path(soup,
-                     path : list,
-                     search_text : str,
-                     context_len : int = 30,
-                     start_from_search : bool = True,
-                     end_after_search : bool = False) -> str:
-    """
-    Traverses the BeautifulSoup tree based on the provided path and returns the search term
-    and some context from the target element.
-    """
-    element = soup
-    for tag, position in path:
-        if tag == '[document]':  # This is the entire document, so just continue
-            continue
-        # Find all children that match the tag
-        matching_children = [child for child in element.children if isinstance(child, Tag) and child.name == tag]
-        if position < len(matching_children):
-            element = matching_children[position]
+        if found:
+            self.searched_text = search_text
+            self.found_element = found_element
+            self.path = path
         else:
-            raise ValueError(f"Cannot navigate to {tag} at position {position}")
+            self.logger.warning("Provided search term was not found in the connected soup!")
 
-    full_text = element.get_text()
-    search_start = full_text.find(search_text)
+    def save_recorded_sbe(self):
 
-    # Extract context around the found text
-    if start_from_search:
-        start = max(0, search_start - context_len)
-    else:
-        start = 0
+        """
+        Saves the path, link, and searched_text to a JSON file.
+        """
+        data = {
+            "path": self.path,
+            "link": self.link,
+            "searched_text": self.searched_text
+        }
+        with open(self.filename, 'w') as f:
+            json.dump(data, f)
+        self.logger.info(f"Data saved to '{self.filename}'")
 
-    if end_after_search:
-        end = min(len(full_text), search_start + len(search_text) + context_len)
-    else:
-        end = len(full_text)
+    def load_recorded_sbe(self):
 
-    return full_text[start:end]
+        """
+        Loads only the path from a JSON file.
+        """
+        with open(self.filename, 'r') as f:
+            data = json.load(f)
+        self.path = data["path"]
+        self.logger.info(f"Path loaded from {self.filename}")
+
+    def show_recorded_sbe(self, filename : str = None):
+
+        """
+        Displays the link, searched_text, and path from a JSON file.
+        """
+        if filename is None:
+            filename = self.filename
+
+        with open(filename, 'r') as f:
+            data = json.load(f)
+
+        print("Link:", data.get("link", "N/A"))
+        print("Searched Text:", data.get("searched_text", "N/A"))
+        print("Path:", data.get("path", []))
 
 
+
+    def extract_from_path(self,
+                        search_text_start : str = '',
+                        search_text_end : str = '',
+                        context_len : int = 0) -> str:
+        """
+        Traverses the BeautifulSoup tree based on the provided path and returns the search term
+        and some context from the target element.
+        """
+
+        if self.soup is None:
+            raise ValueError("Missing 'soup' parameter!")
+
+        if self.path == []:
+            raise ValueError("Missing 'path' parameter!")
+
+
+        element = self.soup
+        for tag, position in self.path:
+            if tag == '[document]':  # This is the entire document, so just continue
+                continue
+            # Find all children that match the tag
+            matching_children = [child for child in element.children \
+                if isinstance(child, Tag) and child.name == tag]
+            if position < len(matching_children):
+                element = matching_children[position]
+            else:
+                raise ValueError(f"Cannot navigate to {tag} at position {position}")
+
+        full_text = element.get_text()
+
+        # Extract context around the found text
+        if search_text_start != '':
+            search_start = full_text.find(search_text_start)
+            start = max(0, search_start - context_len)
+        else:
+            start = 0
+
+
+        if search_text_end != '':
+            search_end = full_text.find(search_text_end)
+
+            end = max(start, search_end + context_len)
+
+        else:
+
+            end = len(full_text)
+
+        return full_text[start:end]
