c2ffee863828621b854605b7eb61c47c0622e0f1
Kiril-Mordan <kiril.mordan@icloud.com>
2023-12-17 22:18:26 +0100

    [REFACTOR] script for extracting requirements with package mapping

--- tools/extract_requirements.py
+++ tools/extract_requirements.py
@@ -1,6 +1,16 @@
 import re
 import os
+import json
+import argparse
 from stdlib_list import stdlib_list
+
+def load_package_mappings(mapping_file):
+    """
+    Get file with mappings for packages which immport names differ from install names.
+    """
+
+    with open(mapping_file, 'r') as file:
+        return json.load(file)
 
 
 def list_custom_modules(custom_modules_path):
@@ -20,31 +30,47 @@ def is_standard_library(module_name, python_version='3.8'):
     """
     return module_name in stdlib_list(python_version)
 
-def extract_requirements(path_to_module, module_name, custom_modules, python_version='3.8'):
+def extract_requirements(path_to_module, module_name, custom_modules,package_mappings = {}, python_version='3.8'):
 
     file_path = f"{path_to_module}/{module_name}.py"
 
-    # Regex pattern for import statements (both 'import' and 'from ... import ...') and version comments
-    pattern = re.compile(r"(?:import (\S+)|from (\S+) import).*?#(?:\s*(==|>=|<=|>|<)\s*([0-9.]+))?")
+    # Separate regex patterns for 'import' and 'from ... import ...' statements
+    import_pattern = re.compile(r"import (\S+)(?:\s+#(?:\s*(==|>=|<=|>|<)\s*([0-9.]+)))?")
+    from_import_pattern = re.compile(r"from (\S+) import.*(?:\s+#(?:\s*(==|>=|<=|>|<)\s*([0-9.]+)))?")
 
-    # init requirements
     requirements = [f'### {module_name}']
 
     with open(file_path, 'r') as file:
         for line in file:
-            match = pattern.match(line)
-            if match:
-                module_import, module_from, version_constraint, version = match.groups()
-                module = module_from if module_from else module_import
+            import_match = import_pattern.match(line)
+            from_import_match = from_import_pattern.match(line)
 
-                # Skip standard library and custom modules
-                if is_standard_library(module) or module in custom_modules:
-                    continue
+            if import_match:
+                module, version_constraint, version = import_match.groups()
+            elif from_import_match:
+                module, version_constraint, version = from_import_match.groups()
+            else:
+                continue
 
-                if version is not None:
-                    requirements.append(f"{module}{version_constraint}{version}")
-                else:
-                    requirements.append(module)
+            # Extract the base package name
+            module = module.split('.')[0]
+
+            # Skip standard library and custom modules
+            if is_standard_library(module, python_version) or module in custom_modules or module == path_to_module:
+                continue
+
+            # Use the mapping to get the correct package name
+            module = package_mappings.get(module, module)
+
+            version_info = f"{version_constraint}{version}" if version_constraint and version else ""
+
+            # Print for debugging
+            # print(f"Matched line: {line.strip()}, Module: {module}, Version: {version_info}")
+
+            if version_info:
+                requirements.append(f"{module}{version_info}")
+            else:
+                requirements.append(module)
 
     return requirements
 
@@ -61,17 +87,28 @@ def write_requirements_file(requirements,
 
 
 if __name__ == "__main__":
-    module_name = "mock_vector_database"
+
+    # Set up argument parser
+    parser = argparse.ArgumentParser(description='Extract requirements from module.')
+    parser.add_argument('module_name', type=str, help='Name of the module')
+    args = parser.parse_args()
+
+    # Use the argument
+    module_name = args.module_name
+
+    mapping_file = 'env_spec/package_mapping.json'
     path_to_module = "python_modules"
     output_path = "env_spec"
     output_file_prefix = "requirements_"
     custom_modules_path = 'python_modules'
     python_version = '3.9'
 
+    package_mappings = load_package_mappings(mapping_file)
     custom_modules = list_custom_modules(custom_modules_path)
     requirements = extract_requirements(path_to_module = path_to_module,
                                         module_name = module_name,
-                                        python_version=python_version,
+                                        python_version = python_version,
+                                        package_mappings = package_mappings,
                                         custom_modules = custom_modules)
 
     write_requirements_file(requirements = requirements,
