36b1464892900dd7316064a41b88939694003d3a
Kiril-Mordan <kiril.mordan@icloud.com>
2023-12-20 02:02:15 +0100

    [REFACTOR] separation if scripts for updating requirements

--- tools/update_requirements.sh
+++ tools/update_requirements.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# Directory containing your Python modules
-modules_directory="python_modules"
-
-# Loop through each module
-for file in "$modules_directory"/*.py
-do
-    module_name=$(basename "$file" .py)  # Extract the module name
-    echo "Processing module: $module_name"
-
-    # Run your Python script here
-    python3 ./tools/extract_requirements.py "$module_name"
-done
-
 requirements_directory="env_spec"  # Directory containing requirements_*.txt files
 combined_requirements="requirements.txt"
 
