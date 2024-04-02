710142c8c62907a50bb4a33baa93e9f0cfed9c1a
Kiril-Mordan <kiril.mordan@icloud.com>
2024-04-02 23:35:13 +0200

    [DRAFT] creating parameter sets and adding to solution locally with parameterframe

--- draft_modules/parameterframe.py
+++ draft_modules/parameterframe.py
@@ -7,6 +7,7 @@ It allows for the structured storage and retrieval of parameter sets from a data
 
 import attr
 import os
+import random
 import dill
 import hashlib
 from datetime import datetime
@@ -19,7 +20,8 @@ import ast
 __design_choices__ = {
     "FileTypeHandler" : ['prepares one parameter file and reconstructs one parameter file at a time',
                          'txt and yaml files can be processed',
-                         'yaml files are not reconstructed 1to1 but are first make into python dictionary, with python type mapping']
+                         'yaml files are not reconstructed 1to1 but are first make into python dictionary, with python type mapping'],
+    "ParameterFrame" : ['parameter_names and paramer_description are optional']
 }
 
 
@@ -451,18 +453,58 @@ class FileTypeHandler:
             with open(file_path, 'w', encoding='utf-8') as file:
                 file.write(self.file_content)
 
+@attr.s
+class ComplexNameGenerator:
+
+    seed = attr.ib(default=None)
+
+    def generate_random_name(self, seed : int = None):
+
+        """
+        Generates random name that is combionation of color, adjective, noun and 3 digits
+        """
+        # Sample lists
+        colors = ["red", "blue", "green", "yellow", "pink", "silver", "purple", "golden"]
+        adjectives = ["fuzzy", "bright", "dark", "shiny", "giant", "tiny", "happy", "sad"]
+        nouns = ["toaster", "refrigerator", "microwave", "laptop", "thermostat", "television", "car", "scooter"]
+
+        if isinstance(seed, int):
+            # Set the seed value
+            random.seed(seed)
+
+        # Randomly choose a word from each list
+        color = random.choice(colors)
+        adj = random.choice(adjectives)
+        noun = random.choice(nouns)
+        digits = random.randint(100, 999)
+        # Combine them to form a name
+        name = f"{color}_{adj}_{noun}_{digits}"
+
+        return name
+
+
+
 
 @attr.s
 class ParameterFrame:
 
     params_path = attr.ib()
+
+    # optional
+    solution_id = attr.ib(default=None, type=str)
     param_names = attr.ib(default=None, type=dict)
     param_descriptions = attr.ib(default=None, type=dict)
+    seed = attr.ib(default=None, type=int)
 
+    # dependancies
     file_type_handler = attr.ib(default=FileTypeHandler)
+    name_generator = attr.ib(default=ComplexNameGenerator)
 
     # inner
-    param_attribute_handlers = attr.ib(default={})
+    solutions = attr.ib(default={})
+    param_sets = attr.ib(default={})
+    param_attributes = attr.ib(default={})
+
 
     logger = attr.ib(default=None)
     logger_name = attr.ib(default='ParameterFrame')
@@ -486,6 +528,15 @@ class ParameterFrame:
             logger.setLevel(self.loggerLvl)
 
             self.logger = logger
+
+    def _generate_unique_id(self, txt : str) -> str:
+
+        """
+        Helper function to generate a unique ID for attribute entries
+        """
+
+        hash_id = hashlib.sha256(str(txt).encode()).hexdigest()
+        return hash_id
 
     def process_parameters_from_files(self,
                            params_path : str = None,
@@ -514,16 +565,347 @@ class ParameterFrame:
 
         for param_name in param_names:
 
-            self.param_attribute_handlers[param_name] = self.file_type_handler(
+            self.param_attributes[param_name] = self.file_type_handler(
                 file_path = param_names[param_name],
                 parameter_name = param_name,
                 parameter_description = param_descriptions[param_name]
             )
 
-            self.param_attribute_handlers[param_name].process_file()
+            self.param_attributes[param_name].process_file()
+
+    def make_parameter_set(self,
+                           parameter_set_name : str = None,
+                           parameter_set_description : str = None,
+                           parameter_names : list = None,
+                           param_attributes : dict = None,
+                           seed : int = None):
+
+        """
+        Use processed parameters to compose parameter set
+        """
+
+        if parameter_set_name is None:
+
+            if seed is None:
+                seed = self.seed
+            parameter_set_name = self.name_generator.generate_random_name(seed = seed)
+
+        if parameter_set_description is None:
+            parameter_set_description = ''
+
+        if param_attributes is None:
+            param_attributes = self.param_attributes
+
+        if parameter_names is None:
+            parameter_names = [pn for pn in param_attributes]
+
+        # generate parameter set id
+        parameter_ids = [param_attributes[pa].parameter_id for pa in param_attributes]
+
+        parameter_set_id = self._generate_unique_id(
+            txt = ''.join(parameter_ids))
+
+        # making parameter set lists
+        parameter_set_description = [{'parameter_set_id' : parameter_set_id,
+                               'parameter_set_name' : parameter_set_name,
+                               'parameter_set_description' : parameter_set_description}]
+
+        parameter_set = [{'parameter_set_id' : parameter_set_id,
+                               'parameter_id' : parameter_id} for parameter_id in parameter_ids]
+
+        # saving parameter set lists
+        self.param_sets[parameter_set_name] = {'parameter_set' : parameter_set,
+                                               'parameter_set_description' : parameter_set_description}
 
 
+    def add_solution_description(self,
+                                    solution_name : str,
+                                    deployment_date : str = None,
+                                    deprication_date : str = None,
+                                    solution_description : str = None,
+                                    solution_id : str = None,
+                                    maintainers : list = None):
+
+        """
+        Add new solution description.
+        """
+
+        if solution_id is None:
+            solution_id = self.solution_id
+
+        # trim solution name
+        solution_name = solution_name[:100]
+
+        if solution_id is None:
+            # if solution id not provided create new
+            solution_id = self._generate_unique_id(
+                txt = self.name_generator.generate_random_name() + solution_name)
 
 
+        if solution_name not in self.solutions.keys():
+            self.solutions[solution_name] = {}
 
+        self.solutions[solution_name]['solution_description'] = {
+            'solution_id' : solution_id,
+            'solution_name' : solution_name,
+            'solution_description' : solution_description,
+            'deployment_date' : deployment_date,
+            'deprication_date' : deprication_date,
+            'maintainers' : maintainers
+        }
+
+    def _get_solution_name_from_memory(self, solution_id : str) -> str:
+
+        """
+        Get solution name from memory from solution id.
+        """
+
+        try:
+            solution_name = [self.solutions[s]['solution_description']['solution_name'] \
+                for s in self.solutions \
+                    if self.solutions[s]['solution_description']['solution_id'] == solution_id][0]
+        except Exception as e:
+            raise ValueError(f"{solution_id} is not in solutions saved to memory!")
+
+        return solution_name
+
+    def _get_parameter_set_name_from_memory(self, parameter_set_id : str) -> str:
+
+        """
+        Get parameter set name from memory from parameter set id.
+        """
+
+        try:
+            parameter_set_name = [self.param_sets[p]['parameter_set_description']['parameter_set_name'] \
+                for p in self.param_sets \
+                    if self.solutions[p]['parameter_set_description']['parameter_set_id'] == parameter_set_id][0]
+        except Exception as e:
+            raise ValueError(f"{parameter_set_id} is not in parameter sets saved to memory!")
+
+        return parameter_set_name
+
+    def _get_parameter_set_id_from_memory(self, parameter_set_name : str) -> str:
+
+        """
+        Get parameter set id from memory from parameter set name.
+        """
+
+        try:
+            parameter_set_id = self.param_sets[parameter_set_name]['parameter_set_description']['parameter_set_id']
+
+        except Exception as e:
+            raise ValueError(f"{parameter_set_name} is not in parameter sets saved to memory!")
+
+        return parameter_set_id
+
+
+    def update_solution_description(self,
+                                    solution_id : str,
+                                    solution_name : str = None,
+                                    deployment_date : str = None,
+                                    deprication_date : str = None,
+                                    solution_description : str = None,
+                                    maintainers : list = None):
+
+        """
+        Update solution description.
+        """
+
+        # get solution name
+        solution_name = self._get_solution_name_from_memory(solution_id = solution_id)
+
+        # update description parameters
+        if solution_name is not None:
+            self.solutions[solution_name]['solution_description']['solution_name'] = solution_name
+        if deployment_date is not None:
+            self.solutions[solution_name]['solution_description']['deployment_date'] = deployment_date
+        if deprication_date is not None:
+            self.solutions[solution_name]['solution_description']['deprication_date'] = deprication_date
+        if solution_description is not None:
+            self.solutions[solution_name]['solution_description']['solution_description'] = solution_description
+        if maintainers is not None:
+            self.solutions[solution_name]['solution_description']['maintainers'] = maintainers
+
+
+    def add_parameter_set_to_solution(self,
+                            solution_id : str = None,
+                            solution_name : str = None,
+                            parameter_set_id : str = None,
+                            parameter_set_name : str = None):
+
+        """
+        Add new solution description.
+        """
+
+        if solution_id is None:
+            solution_id = self.solution_id
+
+        if (solution_id is None) and (solution_name is None):
+            raise ValueError("Provide either solution_id or solution_name!")
+
+        if (parameter_set_id is None) and (parameter_set_name is None):
+            raise ValueError("Provide either parameter_set_id or parameter_set_name!")
+
+        if solution_name is None:
+            solution_name = self._get_solution_name_from_memory(solution_id = solution_id)
+
+        if (parameter_set_id is not None) and (parameter_set_name is None):
+            parameter_set_name = self._get_parameter_set_name_from_memory(
+                parameter_set_id = parameter_set_id)
+
+        if (parameter_set_id is None) and (parameter_set_name is not None):
+            parameter_set_id = self._get_parameter_set_id_from_memory(
+                parameter_set_name = parameter_set_name)
+
+        if solution_name not in self.solutions.keys():
+            self.solutions[solution_name] = {}
+
+        if 'solution_parameter_set' not in self.solutions[solution_name].keys():
+            self.solutions[solution_name]['solution_parameter_set'] = {}
+
+        self.solutions[solution_name]['solution_parameter_set'][parameter_set_name] = {
+            'solution_id' : solution_id,
+            'parameter_set_id' : parameter_set_id,
+            'deployment_status' : "STAGING",
+            'insertion_datetime' : datetime.now()
+        }
+
+    def _change_deployment_status(self,
+                                 deployment_status : str,
+                                 solution_id : str = None,
+                                solution_name : str = None,
+                                parameter_set_id : str = None,
+                                parameter_set_name : str = None,
+                                remote : bool = False):
+
+        """
+        Change deployment status of parameter set.
+        """
+
+        if solution_id is None:
+            solution_id = self.solution_id
+
+        if (solution_id is None) and (solution_name is None):
+            raise ValueError("Provide either solution_id or solution_name!")
+
+        if (parameter_set_id is None) and (parameter_set_name is None):
+            raise ValueError("Provide either parameter_set_id or parameter_set_name!")
+
+        if remote:
+            raise Exception("Connection with parameter storage was not established!")
+        else:
+
+            if solution_name is None:
+                solution_name = self._get_solution_name_from_memory(solution_id = solution_id)
+
+            if (parameter_set_id is not None) and (parameter_set_name is None):
+                parameter_set_name = self._get_parameter_set_name_from_memory(
+                    parameter_set_id = parameter_set_id)
+
+            if (parameter_set_id is None) and (parameter_set_name is not None):
+                parameter_set_id = self._get_parameter_set_id_from_memory(
+                    parameter_set_name = parameter_set_name)
+
+            self.solutions[solution_name]['solution_parameter_set']\
+                [parameter_set_name]['deployment_status'] = deployment_status
+
+    def get_deployment_status(self,
+                                 solution_id : str = None,
+                                solution_name : str = None,
+                                parameter_set_id : str = None,
+                                parameter_set_name : str = None,
+                                remote : bool = False):
+
+        """
+        Get deployment status of parameter set.
+        """
+
+        if solution_id is None:
+            solution_id = self.solution_id
+
+        if (solution_id is None) and (solution_name is None):
+            raise ValueError("Provide either solution_id or solution_name!")
+
+        if (parameter_set_id is None) and (parameter_set_name is None):
+            raise ValueError("Provide either parameter_set_id or parameter_set_name!")
+
+        if remote:
+            raise Exception("Connection with parameter storage was not established!")
+        else:
+
+            if solution_name is None:
+                solution_name = self._get_solution_name_from_memory(solution_id = solution_id)
+
+            if (parameter_set_id is not None) and (parameter_set_name is None):
+                parameter_set_name = self._get_parameter_set_name_from_memory(
+                    parameter_set_id = parameter_set_id)
+
+            if (parameter_set_id is None) and (parameter_set_name is not None):
+                parameter_set_id = self._get_parameter_set_id_from_memory(
+                    parameter_set_name = parameter_set_name)
+
+            return self.solutions[solution_name]['solution_parameter_set']\
+                [parameter_set_name]['deployment_status']
+
+    def change_status_from_staging_to_production(self,
+                                                 solution_id : str = None,
+                                                solution_name : str = None,
+                                                parameter_set_id : str = None,
+                                                parameter_set_name : str = None,
+                                                remote : bool = False):
+
+        """
+        Change deployment status of parameter set from staging to production.
+        """
+
+        current_deployment_status = self.get_deployment_status(
+            solution_id = solution_id,
+            solution_name = solution_name,
+            parameter_set_id = parameter_set_id,
+            parameter_set_name = parameter_set_name,
+            remote = remote
+        )
+
+        if current_deployment_status != "STAGING":
+            raise Exception(f"Current deployment status is {current_deployment_status}!")
+
+        self._change_deployment_status(
+            deployment_status = "PRODUCTION",
+            solution_id = solution_id,
+            solution_name = solution_name,
+            parameter_set_id = parameter_set_id,
+            parameter_set_name = parameter_set_name,
+            remote = remote
+        )
+
+    def change_status_from_production_to_archived(self,
+                                                 solution_id : str = None,
+                                                solution_name : str = None,
+                                                parameter_set_id : str = None,
+                                                parameter_set_name : str = None,
+                                                remote : bool = False):
+
+        """
+        Change deployment status of parameter set from production to archived.
+        """
+
+        current_deployment_status = self.get_deployment_status(
+            solution_id = solution_id,
+            solution_name = solution_name,
+            parameter_set_id = parameter_set_id,
+            parameter_set_name = parameter_set_name,
+            remote = remote
+        )
+
+        if current_deployment_status != "PRODUCTION":
+            raise Exception(f"Current deployment status is {current_deployment_status}!")
+
+        self._change_deployment_status(
+            deployment_status = "ARCHIVED",
+            solution_id = solution_id,
+            solution_name = solution_name,
+            parameter_set_id = parameter_set_id,
+            parameter_set_name = parameter_set_name,
+            remote = remote
+        )
 
