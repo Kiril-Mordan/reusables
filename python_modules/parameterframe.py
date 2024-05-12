cb1d5041b116e7bcffa291851174ed6b97790cdf
Kiril-Mordan <kiril.mordan@icloud.com>
2024-05-12 23:50:23 +0200

    [parameterframe] binary compression and decompression with zlib; optional symmetric encryption and decryption with cryptography lib

--- python_modules/parameterframe.py
+++ python_modules/parameterframe.py
@@ -16,11 +16,18 @@ import yaml
 from collections import defaultdict
 import logging
 import ast
-
+import pandas as pd
 from sqlalchemy import create_engine, Column, String, Text, ForeignKey, DateTime
 from sqlalchemy.orm import relationship, sessionmaker, declarative_base
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError, DBAPIError
 from sqlalchemy import inspect
+# encodeing and compression
+import base64
+import zlib
+# encrypting
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 
 __design_choices__ = {
@@ -419,13 +426,19 @@ class SqlAlchemyDatabaseManager:
         file_name = Column(String)
         file_type = Column(String)
 
+    # class ParameterAttribute(Base):
+    #     __tablename__ = 'parameter_attribute'
+    #     parameter_id = Column(String, ForeignKey('parameter_description.parameter_id'), primary_key=True)
+    #     attribute_id = Column(String, ForeignKey('attribute_values.attribute_id'), primary_key=True)
+    #     previous_attribute_id = Column(String, nullable=True)
+    #     attribute_values = relationship("AttributeValues")
+    #     parameter_description = relationship("ParameterDescription")
+
     class ParameterAttribute(Base):
         __tablename__ = 'parameter_attribute'
-        parameter_id = Column(String, ForeignKey('parameter_description.parameter_id'), primary_key=True)
-        attribute_id = Column(String, ForeignKey('attribute_values.attribute_id'), primary_key=True)
+        parameter_id = Column(String, primary_key=True)
+        attribute_id = Column(String, primary_key=True)
         previous_attribute_id = Column(String, nullable=True)
-        attribute_values = relationship("AttributeValues")
-        parameter_description = relationship("ParameterDescription")
 
     class ParameterSet(Base):
         __tablename__ = 'parameter_set'
@@ -701,6 +714,7 @@ class FileTypeHandler:
 
     # inputs
     file_path = attr.ib(default=None, type = str)
+    password = attr.ib(default=None, type = str)
 
     # inputs for reconstruction
     parameter_name = attr.ib(default='', type = str)
@@ -727,7 +741,6 @@ class FileTypeHandler:
 
     def __attrs_post_init__(self):
         self._initialize_logger()
-
 
     def _initialize_logger(self):
 
@@ -907,13 +920,81 @@ class FileTypeHandler:
         decoded_bytes = dill.loads(encoded_data)
         return decoded_bytes
 
-    def _encode_binary(self, data : str) -> str:
-        """Encode data using Base64."""
-        return str(data)
+    def _compress_string_zlib(self, input_string : str):
+        # Compressing directly from string bytes
+        compressed_data = str(zlib.compress(input_string.encode('utf-8')))
+        return compressed_data
 
-    def _decode_binary(self, encoded_data : str) -> str:
-        """Decode data from Base64."""
-        return ast.literal_eval(encoded_data)
+    def _decompress_zlib(self, compressed_data : bin):
+        # Decompressing and decoding the bytes back to a string
+        return zlib.decompress(compressed_data).decode('utf-8')
+
+    def _derive_key(self,password: str, salt: bytes, key_length=32):
+        """ Derive a key from a password and salt. """
+        kdf = Scrypt(
+            salt=salt,
+            length=key_length,
+            n=2**14,
+            r=8,
+            p=1,
+            backend=default_backend()
+        )
+        return kdf.derive(password.encode())
+
+    def _encrypt_data(self, data, password):
+        """Encrypts data using AES-CBC."""
+
+        salt = os.urandom(16)
+        key = self._derive_key(password, salt=salt)
+
+        iv = os.urandom(16)  # Generate a random IV
+        cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+        encryptor = cipher.encryptor()
+        # Ensure the data is a multiple of 16 in length
+        padded_data = data + (16 - len(data) % 16) * bytes([16 - len(data) % 16])
+        encrypted_data = encryptor.update(padded_data) + encryptor.finalize()
+        return salt + iv + encrypted_data  # Include salt and IV for decryption
+
+    def _decrypt_data(self, encrypted_data, password):
+        """Decrypts data that was encrypted with AES-CBC."""
+        salt = encrypted_data[:16]
+        iv = encrypted_data[16:32]
+        encrypted_data = encrypted_data[32:]
+
+        key = self._derive_key(password,salt)  # Derive the key again with the new salt
+        cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+        decryptor = cipher.decryptor()
+        decrypted_padded_data = decryptor.update(encrypted_data) + decryptor.finalize()
+        pad_length = decrypted_padded_data[-1]  # Remove padding
+        return decrypted_padded_data[:-pad_length]
+
+    def _encode_binary(self, data, password = None):
+        """Encodes binary data to a compressed base64 string, with optional encryption."""
+
+        if password is None:
+            password = self.password
+
+        compressed_data = zlib.compress(data)
+        if password:
+            encrypted_data = self._encrypt_data(compressed_data, password=password)
+            base64_encoded = base64.b64encode(encrypted_data).decode('utf-8')
+        else:
+            base64_encoded = base64.b64encode(compressed_data).decode('utf-8')
+        return base64_encoded
+
+    def _decode_binary(self, encoded_data, password = None):
+        """Decodes a base64 string back to original binary, with optional decryption."""
+
+        if password is None:
+            password = self.password
+
+        data = base64.b64decode(encoded_data.encode('utf-8'))
+        if password:
+            decrypted_data = self._decrypt_data(data, password=password)
+            decompressed_data = zlib.decompress(decrypted_data)
+        else:
+            decompressed_data = zlib.decompress(data)
+        return decompressed_data
 
 
     def _process_dill(self,
@@ -956,7 +1037,7 @@ class FileTypeHandler:
         parameter_attributes = []
         attribute_values = []
 
-        str_content = str(content)
+        str_content = self._encode_binary(data = content)
 
         # Split the text into chunks of chunk_size
         for i in range(0, len(str_content), chunk_size):
@@ -973,7 +1054,7 @@ class FileTypeHandler:
             attribute_values.append({
                 'attribute_id': attribute_id,
                 'attribute_name': f'{int(i//chunk_size)}',
-                'attribute_value': self._encode_binary(data = chunk),
+                'attribute_value':  chunk,
             'attribute_value_type': type(content).__name__
             })
 
@@ -1391,6 +1472,8 @@ class ParameterFrame:
     param_names = attr.ib(default=None, type=dict)
     param_descriptions = attr.ib(default=None, type=dict)
     seed = attr.ib(default=None, type=int)
+    chunk_size = attr.ib(default=255, type=int)
+    password = attr.ib(default=None, type=str)
 
     connection_details = attr.ib(default = {
         'base_url' : "http://localhost:8000"})
@@ -1422,8 +1505,6 @@ class ParameterFrame:
             self.database_connector = MockerDatabaseConnector(connection_details = self.connection_details)
 
         self._initialize_name_generator()
-
-        import pandas as pd
 
         self.pd = pd
         self.pd.set_option('display.max_colwidth', 70)
@@ -1494,7 +1575,9 @@ class ParameterFrame:
             self.param_attributes[param_name] = self.file_type_handler(
                 file_path = param_names[param_name],
                 parameter_name = param_name,
-                parameter_description = param_descriptions[param_name]
+                parameter_description = param_descriptions[param_name],
+                chunk_size = self.chunk_size,
+                password = self.password
             )
 
             self.param_attributes[param_name].process_file()
@@ -1882,7 +1965,9 @@ class ParameterFrame:
                         file_path = file_path,
                         parameter_id = param_id,
                         parameter_attributes_list = parameter_attribute_list,
-                        attribute_values_list = attribute_values_list
+                        attribute_values_list = attribute_values_list,
+                        chunk_size = self.chunk_size,
+                        password = self.password
                     )
             fth.reconstruct_file()
 
