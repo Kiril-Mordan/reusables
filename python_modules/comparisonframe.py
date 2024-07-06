d0b5b39bdd066ec07d003eabace5a77901a2df90
Kiril-Mordan <kiril.mordan@icloud.com>
2024-07-06 04:16:23 +0200

    [comparisonframe] separate expandable records analyser

--- python_modules/comparisonframe.py
+++ python_modules/comparisonframe.py
@@ -26,6 +26,147 @@ __package_metadata__ = {
 }
 
 @attr.s
+class RecordsAnalyser:
+
+    """
+    Calculates metrics and scores based on records in comparisonframe.
+    """
+
+    mocker_h = attr.ib(default = None)
+
+    # Logger settings
+    logger = attr.ib(default=None)
+    logger_name = attr.ib(default='RecordsAnalyser')
+    loggerLvl = attr.ib(default=logging.INFO)
+
+    def __attrs_post_init__(self):
+        self._initialize_logger()
+
+
+    def _initialize_logger(self):
+
+        """
+        Initialize a logger for the class instance based on
+        the specified logging level and logger name.
+        """
+
+        if self.logger is None:
+            logging.basicConfig(level=self.loggerLvl)
+            logger = logging.getLogger(self.logger_name)
+            logger.setLevel(self.loggerLvl)
+
+            self.logger = logger
+    
+
+    def calculate_score(self, method_name : str, *args, **kwargs):
+
+        """
+        Calculates any available score.
+        """
+
+        method = getattr(self, method_name, None)
+        if callable(method):
+            return method(*args, **kwargs)
+        else:
+            raise AttributeError(f"Method '{method_name}' not found")
+
+    def calculate_scores(self, method_names : list, *args, **kwargs):
+
+        """
+        Calculates dictionary of available scores.
+        """
+
+        scores = {}
+        
+        for method_name in method_names:
+
+            scores[method_name] = self.calculate_score(method_name = method_name,
+                                        *args, 
+                                        **kwargs)
+
+        return scores
+
+
+    def compare(self, exp_text : str, prov_text : str, query : str = ''):
+
+        """
+        Performs a detailed comparison between two texts, providing metrics like character count, word count, semantic similarity, etc.
+        """
+
+        results = {
+            'query' : query,
+            'char_count_diff': self.compare_char_count(exp_text, prov_text),
+            'word_count_diff': self.compare_word_count(exp_text, prov_text),
+            'line_count_diff': self.compare_line_count(exp_text, prov_text),
+            'punctuation_diff': self.compare_punctuation(exp_text, prov_text),
+            'semantic_similarity': self.compare_semantic_similarity(exp_text, prov_text),
+            'expected_text' : exp_text,
+            'provided_text' : prov_text
+        }
+
+        return results
+
+    def char_count(self, exp_text, prov_text):
+
+        """
+        Calculates the absolute difference in the number of characters between two texts.
+        """
+
+        return abs(len(exp_text) - len(prov_text))
+
+    def word_count(self, exp_text, prov_text):
+
+        """
+        Calculates the absolute difference in the number of words between two texts.
+        """
+
+        return abs(len(exp_text.split()) - len(prov_text.split()))
+
+    def line_count(self, exp_text, prov_text):
+
+        """
+        Calculates the absolute difference in the number of lines between two texts.
+        """
+
+        return abs(len(exp_text.splitlines()) - len(prov_text.splitlines()))
+
+    def punctuation(self, exp_text, prov_text):
+
+        """
+        Calculates the total difference in the use of punctuation characters between two texts.
+        """
+
+        punctuation1 = Counter(char for char in exp_text if char in string.punctuation)
+        punctuation2 = Counter(char for char in prov_text if char in string.punctuation)
+        return sum((punctuation1 - punctuation2).values()) + sum((punctuation2 - punctuation1).values())
+
+
+    def semantic_similarity(self, exp_text, prov_text):
+
+        """
+        Computes the semantic similarity between two pieces of text using their embeddings.
+        """
+
+
+        self.mocker_h.insert_values(values_dict_list = [
+            {"text" : exp_text}
+            ],
+            var_for_embedding_name = 'text',
+            embed = True)
+
+        self.mocker_h.search_database(
+            query = prov_text,
+            filter_criteria = {
+                "text" : exp_text
+            }
+        )
+
+        distance = self.mocker_h.results_dictances
+
+        return distance[0]
+
+
+@attr.s
 class ComparisonFrame:
 
     """
@@ -42,15 +183,25 @@ class ComparisonFrame:
     mocker_params = attr.ib(default = {
         'file_path' : "./comparisonframe_storage",
          'persist' : True})
-    ## mockerdb instance
+    
+    
+    ## dependencies
     mocker_h_class = attr.ib(default = MockerDB)
-    ## initialized mocker instance
+    records_analyser_class = attr.ib(default = RecordsAnalyser)
+    ## activated dependecies
     mocker_h = attr.ib(default=None)
-
+    records_analyser = attr.ib(default=None)
 
     # Files saved to persist
     record_file = attr.ib(default="record_file.csv")  # file where queries and expected results are stored
     results_file = attr.ib(default="comparison_results.csv") # file where comparison results will be stored
+
+    # Scores to calculate
+    compare_scores = attr.ib(default = ['char_count',
+                                        'word_count',
+                                        'line_count',
+                                        'punctuation',
+                                        'semantic_similarity'])
 
     # Define acceptable margins
     margin_char_count_diff = attr.ib(default=10)
@@ -65,6 +216,7 @@ class ComparisonFrame:
     def __attrs_post_init__(self):
         self._initialize_logger()
         self._initialize_mocker()
+        self._initialize_records_analyser()
         self._initialize_record_file()
 
 
@@ -109,6 +261,15 @@ class ComparisonFrame:
 
         self.mocker_h.establish_connection()
 
+    def _initialize_records_analyser(self):
+
+        """
+        Initializes records analyser.
+        """
+
+        self.records_analyser = self.records_analyser_class(
+            mocker_h = self.mocker_h
+        )
 
 
     def record_query(self, query, expected_text, overwrite=True):
@@ -316,12 +477,16 @@ class ComparisonFrame:
     def compare_with_record(self,
                             query : str,
                             provided_text : str,
+                            compare_scores : list = None,
                             mark_as_tested : bool = True,
                             return_results : bool = False):
 
         """
         Compares the provided text with all recorded expected results for a specific query and stores the comparison results.
         """
+
+        if compare_scores is None:
+            compare_scores = self.compare_scores
 
         # Read the recorded data and find all records for the query, sorted by timestamp
         with open(self.record_file, mode='r', encoding='utf-8') as file:
@@ -339,16 +504,28 @@ class ComparisonFrame:
         comparisons = []
         for record in records:
             expected_text = record[3]  # expected text is in the fourth column
-            comparison = self.compare(expected_text, provided_text, query=query)
+
+            comparison = {}
             comparison['id'] = record[0]  # id is in the first column
+            comparison['query'] = query
+            comparison['expected_text'] = expected_text
+            comparison['provided_text'] = provided_text
+
+
+            comparison_scores = self.records_analyser.calculate_scores(
+                method_names = compare_scores, 
+                exp_text = expected_text,
+                prov_text = provided_text)
+            comparison.update(comparison_scores)
+
             comparisons.append(comparison)
 
 
         # After conducting the comparison
         for comparison in comparisons:
             # Check if differences are within acceptable margins
-            passed_char_count = comparison['char_count_diff'] <= self.margin_char_count_diff
-            passed_word_count = comparison['word_count_diff'] <= self.margin_word_count_diff
+            passed_char_count = comparison['char_count'] <= self.margin_char_count_diff
+            passed_word_count = comparison['word_count'] <= self.margin_word_count_diff
             passed_semantic_similarity = comparison['semantic_similarity'] >= self.margin_semantic_similarity
 
             # If all checks pass, mark as 'pass'; otherwise, 'fail'
@@ -371,81 +548,3 @@ class ComparisonFrame:
 
         if return_results:
             return results_df
-
-    def compare(self, exp_text : str, prov_text : str, query : str = ''):
-
-        """
-        Performs a detailed comparison between two texts, providing metrics like character count, word count, semantic similarity, etc.
-        """
-
-        results = {
-            'query' : query,
-            'char_count_diff': self.compare_char_count(exp_text, prov_text),
-            'word_count_diff': self.compare_word_count(exp_text, prov_text),
-            'line_count_diff': self.compare_line_count(exp_text, prov_text),
-            'punctuation_diff': self.compare_punctuation(exp_text, prov_text),
-            'semantic_similarity': self.compare_semantic_similarity(exp_text, prov_text),
-            'expected_text' : exp_text,
-            'provided_text' : prov_text
-        }
-
-        return results
-
-    def compare_char_count(self, exp_text, prov_text):
-
-        """
-        Calculates the absolute difference in the number of characters between two texts.
-        """
-
-        return abs(len(exp_text) - len(prov_text))
-
-    def compare_word_count(self, exp_text, prov_text):
-
-        """
-        Calculates the absolute difference in the number of words between two texts.
-        """
-
-        return abs(len(exp_text.split()) - len(prov_text.split()))
-
-    def compare_line_count(self, exp_text, prov_text):
-
-        """
-        Calculates the absolute difference in the number of lines between two texts.
-        """
-
-        return abs(len(exp_text.splitlines()) - len(prov_text.splitlines()))
-
-    def compare_punctuation(self, exp_text, prov_text):
-
-        """
-        Calculates the total difference in the use of punctuation characters between two texts.
-        """
-
-        punctuation1 = Counter(char for char in exp_text if char in string.punctuation)
-        punctuation2 = Counter(char for char in prov_text if char in string.punctuation)
-        return sum((punctuation1 - punctuation2).values()) + sum((punctuation2 - punctuation1).values())
-
-
-    def compare_semantic_similarity(self, exp_text, prov_text):
-
-        """
-        Computes the semantic similarity between two pieces of text using their embeddings.
-        """
-
-
-        self.mocker_h.insert_values(values_dict_list = [
-            {"text" : exp_text}
-            ],
-            var_for_embedding_name = 'text',
-            embed = True)
-
-        self.mocker_h.search_database(
-            query = prov_text,
-            filter_criteria = {
-                "text" : exp_text
-            }
-        )
-
-        distance = self.mocker_h.results_dictances
-
-        return distance[0]
