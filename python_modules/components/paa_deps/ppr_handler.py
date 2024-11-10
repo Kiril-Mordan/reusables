8a576a05614d4dbabc1f722058c10cf8124b2588
Kiril-Mordan <kiril.mordan@icloud.com>
2024-11-10 23:53:09 +0100

    [package_auto_assembler] scripts to run pylint check and convert drawio to png accessible through ppr_handler

--- python_modules/components/paa_deps/ppr_handler.py
+++ python_modules/components/paa_deps/ppr_handler.py
@@ -1,6 +1,10 @@
 import logging
 import os
-import shutil
+import subprocess
+import importlib
+import importlib.metadata
+import importlib.resources as pkg_resources
+import csv
 import attr #>=22.2.0
 
 @attr.s
@@ -12,6 +16,12 @@ class PprHandler:
 
     # inputs
     paa_dir = attr.ib(default=".paa")
+
+    module_dir = attr.ib(default=None)
+    drawio_dir = attr.ib(default=None)
+    docs_dir = attr.ib(default=None)
+
+    pylint_threshold = attr.ib(default=None)
 
     # processed
     logger = attr.ib(default=None)
@@ -38,10 +48,12 @@ class PprHandler:
 
         os.makedirs(paa_dir)
 
-        with open(os.path.join(paa_dir, 'package_licenses.json'), 'w') as init_file:
+        with open(os.path.join(paa_dir, 'package_licenses.json'),
+        'w', encoding = 'utf-8') as init_file:
             init_file.write("{}")
 
-        with open(os.path.join(paa_dir, 'package_mapping.json'), 'w') as init_file:
+        with open(os.path.join(paa_dir, 'package_mapping.json'),
+        'w', encoding = 'utf-8') as init_file:
             init_file.write("{}")
 
     def _create_empty_tracking_files(self, paa_dir : str):
@@ -52,9 +64,9 @@ class PprHandler:
             'w') as file:
             file.write("")
 
-        log_file = open(os.path.join(paa_dir,'tracking','version_logs.csv'), 
-        'a', 
-        newline='', 
+        log_file = open(os.path.join(paa_dir,'tracking','version_logs.csv'),
+        'a',
+        newline='',
         encoding="utf-8")
         csv_writer = csv.writer(log_file)
         csv_writer.writerow(['Timestamp', 'Package', 'Version'])
@@ -77,8 +89,8 @@ class PprHandler:
             'package-auto-assembler'
         ]
 
-        with open(os.path.join(paa_dir,'requirements', 'requirements_dev.txt'), 
-        'w') as file:
+        with open(os.path.join(paa_dir,'requirements', 'requirements_dev.txt'),
+        'w', encoding = "utf-8") as file:
             for req in init_requirements:
                 file.write(req + '\n')
 
@@ -106,14 +118,128 @@ class PprHandler:
                 os.makedirs(os.path.join(paa_dir,'docs'))
         except Exception as e:
             self.logger.warning("Failed to initialize paa dir!")
-            self.logger.error(f"e")
+            self.logger.error(e)
             return False
 
         return True
-            
+
+    def _remove_trailing_whitespace_from_file(self, file_path : str):
+        with open(file_path, 'r', encoding = "utf-8") as file:
+            lines = file.readlines()
+
+        # Remove trailing whitespace from each line
+        cleaned_lines = [line.rstrip() + '\n' for line in lines]
+
+        # Write the cleaned lines back to the file
+        with open(file_path, 'w', encoding = "utf-8") as file:
+            file.writelines(cleaned_lines)
+
+        self.logger.debug(f"Cleaned {file_path}")
+
+    def _remove_trailing_whitespace_from_directory(self, directory : str):
+        for root, _, files in os.walk(directory):
+            for file in files:
+                if file.endswith('.py'):
+                    file_path = os.path.join(root, file)
+                    self._remove_trailing_whitespace_from_file(file_path)
+
+    def remove_trailing_whitespaces(self, file_dir_path : str):
+
+        """
+        Removes trailing whitespaces 
+        from a given file or files in a directory.
+        """
+
+        if os.path.isfile(file_dir_path):
+            # If it's a file, clean just that file
+            if file_dir_path.endswith('.py'):
+                self._remove_trailing_whitespace_from_file(file_dir_path)
+            else:
+                self.logger.error(f"{file_dir_path} is not a Python file.")
+        elif os.path.isdir(file_dir_path):
+            # If it's a directory, clean all .py files within it
+            self._remove_trailing_whitespace_from_directory(file_dir_path)
+        else:
+            self.logger.error(f"{file_dir_path} is not a valid file or directory.")
+
+
+    def run_pylint_tests(self, 
+                         module_dir : str = None,
+                         pylint_threshold : str = None,
+                         files_to_check : list = None):
+
+        """
+        Run pylint tests for a given file, files or files in a directory.
+        """
+
+        if module_dir is None:
+            module_dir = self.module_dir
+
+        if pylint_threshold is None:
+            pylint_threshold = self.pylint_threshold
+
+        paa_path = pkg_resources.files('package_auto_assembler')
+
+        if not os.path.exists(paa_path):
+            return 1
+
+        script_path = os.path.join(paa_path,
+                                   "artifacts",
+                                   "tools",
+                                   "pylint_test.sh")
+
+        if not os.path.exists(script_path):
+            return 2
+
+        list_of_cmds = [script_path, 
+                        "--module-directory",
+                        module_dir]
+
+        if pylint_threshold:
+            list_of_cmds += ["--threshold", pylint_threshold]
+
+        if files_to_check:
+            list_of_cmds += files_to_check
+
+        subprocess.run(list_of_cmds, check=True)
+
+        return 0
 
 
 
+    def convert_drawio_to_png(self,
+                              module_name : str = None,
+                              drawio_dir : str = None,
+                              docs_dir : str = None):
 
+        """
+        Converts drawio files in ppr into png files for a package.
+        """
 
+        if drawio_dir is None:
+            drawio_dir = self.drawio_dir
 
+        if docs_dir is None:
+            docs_dir = self.docs_dir
+
+        paa_path = pkg_resources.files('package_auto_assembler')
+
+        if not os.path.exists(paa_path):
+            return 1
+
+        script_path = os.path.join(paa_path,
+                                   "artifacts",
+                                   "tools",
+                                   "convert_drawio_to_png.sh")
+
+        if not os.path.exists(script_path):
+            return 2
+
+        list_of_cmds = [script_path, drawio_dir, docs_dir]
+
+        if module_name:
+            list_of_cmds.append(os.path.join(drawio_dir, f"{module_name}.drawio"))
+
+        subprocess.run(list_of_cmds, check=True)
+
+        return 0
